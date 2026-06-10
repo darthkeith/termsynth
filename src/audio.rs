fb7b6ad8c55d1dee87c4af03e73f11bcee83600d
Keith Kaplan <keithkaplan0@gmail.com>
2026-06-10 04:33:08 -0700

    Replace rodio with cpal

--- src/audio.rs
+++ src/audio.rs
@@ -1,12 +1,12 @@
-use rodio::{
-    DeviceSinkBuilder, Player, source::SineWave, stream::MixerDeviceSink,
+use cpal::{
+    Sample, Stream,
+    traits::{DeviceTrait, HostTrait, StreamTrait},
 };
 
 const FREQ: f32 = 440.0;
 
 pub struct Audio {
-    _handle: MixerDeviceSink,
-    player: Player,
+    stream: Stream,
 }
 
 pub enum Command {
@@ -15,22 +15,42 @@ pub enum Command {
     None,
 }
 
+fn write_silence(data: &mut [f32], _: &cpal::OutputCallbackInfo) {
+    for sample in data.iter_mut() {
+        *sample = Sample::EQUILIBRIUM;
+    }
+}
+
 impl Audio {
     pub fn new() -> Self {
-        let _handle = DeviceSinkBuilder::open_default_sink()
-            .expect("failed to open default audio stream");
-        let player = Player::connect_new(&_handle.mixer());
-        let source = SineWave::new(FREQ);
-        player.pause();
-        player.append(source);
-        Self { _handle, player }
+        let host = cpal::default_host();
+        let device = host
+            .default_output_device()
+            .expect("no output device available");
+        let config = device
+            .default_output_config()
+            .expect("no default output config");
+        match config.sample_format() {
+            cpal::SampleFormat::F32 => (),
+            _ => panic!("unsupported sample format"),
+        }
+        let stream = device
+            .build_output_stream(
+                config.into(),
+                write_silence,
+                |err| eprintln!("audio stream error: {}", err),
+                None,
+            )
+            .unwrap();
+        stream.play().unwrap();
+        Self { stream }
     }
 }
 
 pub fn execute_command(command: Command, audio: &Audio) {
     match command {
-        Command::PlayNote => audio.player.play(),
-        Command::StopNote => audio.player.pause(),
+        Command::PlayNote => (),
+        Command::StopNote => (),
         Command::None => (),
     }
 }
