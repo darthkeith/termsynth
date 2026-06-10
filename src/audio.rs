c49d4c0506d7b3d53bce3be257daad1a833a19f6
Keith Kaplan <keithkaplan0@gmail.com>
2026-06-10 05:32:53 -0700

    Add sine wave output controlled by toggle

--- src/audio.rs
+++ src/audio.rs
@@ -1,24 +1,27 @@
+use std::{
+    f32::consts::PI,
+    sync::{
+        Arc,
+        atomic::{AtomicBool, Ordering},
+    },
+};
+
 use cpal::{
-    Sample, Stream,
+    Stream,
     traits::{DeviceTrait, HostTrait, StreamTrait},
 };
 
 const FREQ: f32 = 440.0;
 
 pub struct Audio {
-    stream: Stream,
+    _stream: Stream,
+    is_on: Arc<AtomicBool>,
 }
 
 pub enum Command {
     PlayNote,
     StopNote,
     None,
-}
-
-fn write_silence(data: &mut [f32], _: &cpal::OutputCallbackInfo) {
-    for sample in data.iter_mut() {
-        *sample = Sample::EQUILIBRIUM;
-    }
 }
 
 impl Audio {
@@ -34,23 +37,43 @@ impl Audio {
             cpal::SampleFormat::F32 => (),
             _ => panic!("unsupported sample format"),
         }
-        let stream = device
+        let is_on = Arc::new(AtomicBool::new(false));
+        let write_sin = {
+            let is_on = is_on.clone();
+            let mut phase = 0.0;
+            let sample_rate = config.sample_rate() as f32;
+            let phase_increment = 1.0 / sample_rate;
+            move |data: &mut [f32], _: &cpal::OutputCallbackInfo| {
+                if is_on.load(Ordering::Relaxed) {
+                    for sample in data.iter_mut() {
+                        *sample = (2.0 * PI * FREQ * phase).sin();
+                        phase = (phase + phase_increment) % 1.0;
+                    }
+                } else {
+                    phase = 0.0;
+                    for sample in data.iter_mut() {
+                        *sample = 0.0;
+                    }
+                }
+            }
+        };
+        let _stream = device
             .build_output_stream(
                 config.into(),
-                write_silence,
+                write_sin,
                 |err| eprintln!("audio stream error: {}", err),
                 None,
             )
             .unwrap();
-        stream.play().unwrap();
-        Self { stream }
+        _stream.play().unwrap();
+        Self { _stream, is_on }
     }
 }
 
 pub fn execute_command(command: Command, audio: &Audio) {
     match command {
-        Command::PlayNote => (),
-        Command::StopNote => (),
+        Command::PlayNote => audio.is_on.store(true, Ordering::Relaxed),
+        Command::StopNote => audio.is_on.store(false, Ordering::Relaxed),
         Command::None => (),
     }
 }
